3c7d8f2496900965ba75f3d2324aec00beb135e7
James You <james.you@protonmail.com>
2020-06-25 03:14:06 -0400

    3rd time's the charm?

--- build.sbt
+++ build.sbt
@@ -1,13 +1,16 @@
 
-
-val dottyVersion = "0.25.0-RC2"
+val dottyVersion = dottyLatestNightlyBuild.get
 val jarName = "dotty-taster-plugin.jar"
+
 lazy val taster = project
   .in(file("."))
   .settings(
     name := "dotty-taster",
     scalaVersion := dottyVersion,
     libraryDependencies += "ch.epfl.lamp" %% "dotty" % dottyVersion,
+    scalacOptions ++= Seq(
+      "-Xplugin:dotty-taster-plugin.jar"
+    ),
     test.in(assembly) := {},
     assemblyOutputPath.in(assembly) := baseDirectory.value / jarName
   )
--- src/main/scala/taster/Taster.scala
+++ src/main/scala/taster/Taster.scala
@@ -4,15 +4,16 @@ import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Phases
 import dotty.tools.dotc.core.Phases.Phase
 import dotty.tools.dotc.plugins.ResearchPlugin
-import dotty.tools.dotc.transform.Pickler
+import dotty.tools.dotc.transform.{Erasure, Pickler}
 
 class Taster extends ResearchPlugin {
   val name = "dotty-taster"
   override val description = "Research plugin for outputting tasty"
+  override val optionsHelp: Option[String] = None
 
   def init(options: List[String], plan: List[List[Phase]])(implicit ctx: Context): List[List[Phase]] = {
-    var newPlan = plan.map((phases: List[Phase]) => phases.filterNot(_.phaseName == Pickler.name))
-    throw new Exception(s"the plan is $newPlan")
-    newPlan
+    val planWithoutPickle = plan.map((phases: List[Phase]) => phases.filterNot(_.phaseName == Pickler.name))
+    val (before, after) = planWithoutPickle.span(ps => !ps.exists(_.phaseName == Erasure.name))
+    before ++ List(List(new Pickler)) ++ after
   }
 }
