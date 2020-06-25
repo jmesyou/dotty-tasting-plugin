2673cf6e8d7916e76a407acd5cd32e33137c5338
James You <james.you@protonmail.com>
2020-06-25 01:57:30 -0400

    eliminate the pickling stage

--- src/main/scala/taster/Taster.scala
+++ src/main/scala/taster/Taster.scala
@@ -4,13 +4,14 @@ import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Phases
 import dotty.tools.dotc.core.Phases.Phase
 import dotty.tools.dotc.plugins.ResearchPlugin
+import dotty.tools.dotc.transform.Pickler
 
 class Taster extends ResearchPlugin {
   val name = "dotty-taster"
   override val description = "Research plugin for outputting tasty"
 
   def init(options: List[String], plan: List[List[Phase]])(implicit ctx: Context): List[List[Phase]] = {
-    print(plan)
-    plan
+    var newPlan = plan.map((phases: List[Phase]) => phases.filterNot(_ == Pickler))
+    newPlan
   }
 }
